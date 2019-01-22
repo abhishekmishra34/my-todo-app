a5d97b724582854a7a0c8bd27af4b34c8ab7a634
abhishekmishra34 <akm77492@gmail.com>
2019-01-22 11:23:13 -0800

    Add task

--- app/src/main/java/com/example/user/mytodoapp/MainActivity.java
+++ app/src/main/java/com/example/user/mytodoapp/MainActivity.java
@@ -5,6 +5,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -25,7 +27,31 @@ public class MainActivity extends AppCompatActivity {
         View view = getLayoutInflater().inflate(R.layout.layout_add_task, null);
         AlertDialog.Builder builder = new AlertDialog.Builder(this);
         builder.setView(view);
-        AlertDialog ad = builder.create();
+        final AlertDialog ad = builder.create();
         ad.show();
+
+        final EditText editTextTitle = view.findViewById(R.id.editTextTitle);
+        final EditText editTextDesc = view.findViewById(R.id.editTextDesc);
+        final EditText editTextFinishBy = view.findViewById(R.id.editTextFinishBy);
+
+        view.findViewById(R.id.buttonAdd).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                String title = editTextTitle.getText().toString().trim();
+                String desc = editTextDesc.getText().toString().trim();
+                String finishBy = editTextFinishBy.getText().toString().trim();
+
+                Task task = new Task(0, title, desc, finishBy, false);
+
+                DbHandler dbHandler = new DbHandler(MainActivity.this);
+                dbHandler.addTask(task);
+
+                Toast.makeText(MainActivity.this, "Task Added", Toast.LENGTH_LONG).show();
+
+                ad.dismiss();
+            }
+        });
+
     }
 }
